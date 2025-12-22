4e5283ec32ab97da02ba10f6c263a0ceed7fdd79
o.kilic <o.kilic@zs-paletten.de>
2025-12-22 22:18:35 +0100

    change to new db structure

--- get-customer-cases/dynamodb.js
+++ get-customer-cases/dynamodb.js
@@ -1,18 +1,37 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-const { DynamoDBDocumentClient, ScanCommand, QueryCommand, GetCommand, PutCommand } = require("@aws-sdk/lib-dynamodb");
+const { DynamoDBDocumentClient, QueryCommand } = require("@aws-sdk/lib-dynamodb");
 
-const client = new DynamoDBClient({ 
-    region: "eu-central-1",
-    credentials: { accessKeyId: "AKIA2UK6GZDT6GMBXRND", secretAccessKey: "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"} 
-});
+const client = new DynamoDBClient({ region: "eu-central-1", 
+  credentials: { accessKeyId: "AKIA2UK6GZDT6GMBXRND", secretAccessKey: "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"} });
 const ddbDocClient = DynamoDBDocumentClient.from(client);
 
-const TABLE_NAME = "Kunden"; // Name deiner DynamoDB-Tabelle
+const TABLE_NAME = "Customer2CaseAndType";
 
-async function getCustomerCases() {
-  const command = new ScanCommand({ TableName: "CustomerCase" });
+async function getAllSortedByOrderId() {
+  const command = new QueryCommand({
+    TableName: TABLE_NAME,
+    KeyConditionExpression: "#pk = :pk AND begins_with(#sk, :sk)",
+    ExpressionAttributeNames: {
+      "#pk": "pk",
+      "#sk": "sk",
+    },
+    ExpressionAttributeValues: {
+      ":pk": "CustomerCase",
+      ":sk": "case#",
+    },
+  });
+
   const result = await ddbDocClient.send(command);
-  return result.Items || [];
+  const items = result.Items ?? [];
+
+  // Sortieren nach orderId (aufsteigend)
+  items.sort((a, b) => {
+    const aId = a.order || 0; // 0 für undefined
+    const bId = b.order || 0;
+    return aId - bId; // aufsteigend
+  });
+
+  return items;
 }
 
-module.exports = { getCustomerCases };
+module.exports = { getAllSortedByOrderId };
